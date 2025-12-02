d525c4095ae0cff51b0609e4158004dd1a1a879a
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2025-12-02 21:17:34 +0200

    Add FavoriteDtos

---
+++ Application/Favorites/DTOs/FavoriteProductDto.cs
@@ -0,0 +1,10 @@
+﻿using Ecom.Application.Products.DTOs;
+using System;
+
+namespace Application.Favorites.DTOs
+{
+    public record FavoriteProductDto : ProductDTO
+    {
+        public DateTime DateAdded { get; init; }
+    }
+}
