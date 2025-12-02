4e948c1ac9104a09772190b46ff1667b3ae66da6
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2025-12-02 21:17:00 +0200

    Add favorite Table and config

---
+++ Core/Entities/Favorite.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Favorite : BaseEntity
+    {
+        public required string AppUserId { get; set; }
+        public int ProductId { get; set; }
+
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey(nameof(AppUserId))]
+        public AppUser AppUser { get; set; } = null!;
+
+        [ForeignKey(nameof(ProductId))]
+        public Core.Entities.Product.Product Product { get; set; } = null!;
+
+        [SetsRequiredMembers]
+        public Favorite(string appUserId, int productId)
+        {
+            AppUserId = appUserId;
+            ProductId = productId;
+        }
+
+        public Favorite() { }
+    }
+}
---
+++ Infrastructure/Config/FavoriteConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Config
+{
+    public class FavoriteConfiguration : IEntityTypeConfiguration<Favorite>
+    {
+        public void Configure(EntityTypeBuilder<Favorite> builder)
+        {
+            builder.HasIndex(f => new { f.AppUserId, f.ProductId })
+          .IsUnique();
+
+            builder.HasOne(f => f.AppUser)
+          .WithMany()
+          .HasForeignKey(f => f.AppUserId);
+        }
+    }
+}
