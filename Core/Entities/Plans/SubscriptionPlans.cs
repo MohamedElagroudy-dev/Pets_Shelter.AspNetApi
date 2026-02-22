caa609e7cc0800d16f761b52f639bab1541be0d6
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2026-02-22 20:51:44 +0200

    Add SubscriptionPlan entity with PlanType enum and pricing properties

---
+++ Core/Entities/Plans/SubscriptionPlans.cs
@@ -0,0 +1,24 @@
+﻿using Core.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Plans
+{
+    public class SubscriptionPlan
+    {
+        public required int Id { get; set; }
+
+        public required string Name { get; set; }          // Bronze Paws
+        public decimal Price { get; set; }        // 300
+        public int DurationInMonths { get; set; } // 3
+
+        public string? Description { get; set; }  // optional
+
+        public required PlanType PlanType { get; set; }    // Adoption / Foster / Sponsor
+
+        public ICollection<PlanFeature>? Features { get; set; }
+    }
+}
