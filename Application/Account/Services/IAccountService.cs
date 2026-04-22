607cd53c8cbd5006f360bb9eae8f2dbf5a2d8f5e
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2026-04-22 20:53:15 +0200

    feat: add UpdateProfile method to IAccountService
    Returns Task<UserInfoDto> and accepts ProfileUpdateDto

--- Application/Account/Services/IAccountService.cs
+++ Application/Account/Services/IAccountService.cs
@@ -17,6 +17,7 @@ namespace Application.Account.Services
         Task<string?> UnassignRoleAsync(UnassignRoleModel model);
         Task<AddressDto> CreateOrUpdateAddress(AddressDto dto);
         Task<UserInfoDto> GetUserInfo();
+        Task<UserInfoDto> UpdateProfile(ProfileUpdateDto dto);
 
         Task<AuthModel?> RefreshToken(string token);
         Task<bool> RevokeToken(string token);
