b06f823a11f6d61785584b776e84f2b50b4d7d95
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2026-04-29 23:33:33 +0300

    Add Foster Dtos

---
+++ Application/FosterAnimals/DTOs/PhotoDTO.cs
@@ -0,0 +1,9 @@
+namespace Ecom.Application.FosterAnimals.DTOs
+{
+    public record PhotoDTO
+    {
+        public int Id { get; init; }
+        public int AnimalId { get; init; }
+        public string ImageUrl { get; init; } = string.Empty;
+    }
+}
---
+++ Application/FosterAnimals/DTOs/UpdateFosterAnimalDTO.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Core.Constants;
+
+namespace Ecom.Application.FosterAnimals.DTOs
+{
+    public record UpdateFosterAnimalDTO
+    {
+        public int Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public double AgeYears { get; init; }
+        public AnimalSize Size { get; init; }
+        public double WeightKg { get; init; }
+        public Gender Gender { get; init; }
+        public int PetTypeId { get; init; }
+        public IFormFileCollection? Photos { get; init; }
+        public int AnimalsFriendlyLevel { get; init; }
+        public int ChildrenFriendlyLevel { get; init; }
+        public int HouseTrainedLevel { get; init; }
+        public DateTime? FosterStartDate { get; init; }
+        public DateTime? FosterEndDate { get; init; }
+        public string? FosterNotes { get; init; }
+        public FosterStatus Status { get; init; }
+        public bool IsUrgent { get; init; }
+    }
+}
