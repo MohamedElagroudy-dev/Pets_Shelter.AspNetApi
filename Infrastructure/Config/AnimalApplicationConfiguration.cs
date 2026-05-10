ad1549317fba0263aa96ffe182cdc8dd6775a1b1
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2026-05-11 00:19:12 +0300

    rename AdoptionApplication to AnimalApplication and add ApplicationType property
    - Renamed class and namespace from AdoptionApp to AnimalApplication
    - Changed Animal navigation property from AdoptionAnimal to BaseAnimal
    - Added ApplicationType property defaulting to Adoption
    - Preserves full backward compatibility with existing adoption data"

---
+++ Infrastructure/Config/AnimalApplicationConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Core.Entities.AdoptionApp;
+using Core.Entities.Animal;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Config
+{
+    public class AnimalApplicationConfiguration : IEntityTypeConfiguration<AdoptionApplication>
+    {
+        public void Configure(EntityTypeBuilder<AdoptionApplication> builder)
+        {
+            builder.ToTable("AnimalApplications");
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.ApplicationType)
+                   .IsRequired();
+
+            builder.HasOne<BaseAnimal>(a => a.Animal)
+                   .WithMany()
+                   .HasForeignKey(a => a.AnimalId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(a => a.Applicant)
+                   .WithMany()
+                   .HasForeignKey(a => a.ApplicantId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
