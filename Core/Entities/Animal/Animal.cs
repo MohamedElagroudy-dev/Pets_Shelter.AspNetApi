c969c841e3fa117f37074c094c236dfb75f8b7e4
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2026-05-10 19:38:46 +0300

    Remove obsolete Animal file

--- Core/Entities/Animal/Animal.cs
+++
@@ -1,4 +0,0 @@
-﻿// This file is now obsolete. The Animal class has been split into BaseAnimal, AdoptionAnimal, and FosterAnimal.
-// Please use AdoptionAnimal or FosterAnimal instead.
-
-// This file is intentionally left blank to avoid duplicate type definitions during migration/refactor.
