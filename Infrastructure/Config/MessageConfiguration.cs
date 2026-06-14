1730cb3988cc8dfd726bf76a35c525a0eda02801
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2026-06-14 23:44:26 +0300

    configure chat relationships and indexes

---
+++ Infrastructure/Config/MessageConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities.Chat;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Config
+{
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.HasKey(m => m.Id);
+
+            builder.Property(m => m.Content)
+                   .HasMaxLength(4000)
+                   .IsRequired();
+
+            // Sender FK
+            builder.HasOne(m => m.Sender)
+                   .WithMany(u => u.SentMessages)
+                   .HasForeignKey(m => m.SenderId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            // Index for loading messages in a room ordered by date
+            builder.HasIndex(m => new { m.ChatRoomId, m.SentAt });
+        }
+    }
+}
