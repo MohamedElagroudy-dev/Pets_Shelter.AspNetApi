c341b1909e7b0e31033513bbe61b99aa19f72686
MohamedElagroudy-dev <Mohamed.Elagroudy.dev@gmail.com>
2026-06-24 00:01:09 +0300

    Add donation payment processing functionality
    Introduces CreateDonationPaymentDto for encapsulating donation payment data.
    Adds ToEntity extension method to convert DTO to Donation entity.
    Updates DonationService with CreateDonationPaymentAsync and
    HandleDonationCompleted methods for processing donations.
    Modifies IDonationService interface to include new methods.

---
+++ Application/UserDonations/DTOs/CreateDonationPaymentDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UserDonations.DTOs
+{
+    public class CreateDonationPaymentDto
+    {
+        public int DonationAnimalId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string? Message { get; set; }
+
+        public bool IsAnonymous { get; set; }
+    }
+}
---
+++ Application/UserDonations/Mappings/DonationMappingExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Application.Account;
+using Application.UserDonations.DTOs;
+using Core.Constants;
+using Core.Entities.Animal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UserDonations.Mappings
+{
+    public static class DonationMappingExtensions
+    {
+        //public static UserDonationDto ToUserDonationDto(this Core.Entities.Donation donation)
+        //{
+        //    if (donation == null) return null;
+        //    return new UserDonationDto
+        //    {
+        //        Id = donation.Id,
+        //        UserId = donation.UserId,
+        //        Amount = donation.Amount,
+        //        DonationDate = donation.DonationDate,
+        //        DonationType = donation.DonationType,
+        //        PaymentMethod = donation.PaymentMethod,
+        //        TransactionId = donation.TransactionId,
+        //        Status = donation.Status
+        //    };
+        //}
+        public static Donation ToEntity(this CreateDonationPaymentDto dto, CurrentUser currentUser)
+        {
+            var donation = new Donation
+            {
+                DonationAnimalId = dto.DonationAnimalId,
+
+                DonorId = currentUser.Id,
+
+                Amount = dto.Amount,
+
+                Message = dto.Message,
+
+                IsAnonymous = dto.IsAnonymous,
+
+                DonorName = dto.IsAnonymous
+                    ? null
+                    : $"{currentUser.FirstName} {currentUser.LastName}",
+
+                PaymentStatus = DonationPaymentStatus.Pending,
+
+                DonatedAt = DateTime.UtcNow
+            };
+            return donation;
+        }
+    }
+}
